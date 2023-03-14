9cf188752783436bbe50bfcfbec793dd5750d2ca
Alex Servirog <servalex1@gmail.com>
2023-03-14 02:23:51 +0200

    using aiohttp for requests

--- pygame/game.py
+++ pygame/game.py
@@ -1,21 +1,10 @@
-"""
 import pygame
+import aiohttp
+import asyncio
 
 pygame.joystick.init()
-
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-
 print(joysticks)
-
-"""
-import pygame
-
-pygame.joystick.init()
-
-joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
-
-print(joysticks)
-
 print("main part")
 
 pygame.init()
@@ -26,26 +15,75 @@ rect = pygame.Rect(0, 0, 20, 20)
 rect.center = window.get_rect().center
 vel = 5
 
+ip = "http://192.168.100.186/"
+
+
+
+currentButtonStates = [False, False, False, False]
+
+async def aioRequest(command):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(ip + command) as resp:
+			pass
+			#print(resp.status)
+			#print(await resp.text())
+
+def sendCommand(command):
+	print(command)
+	asyncio.run(aioRequest(command))
+
+
+def sendSomeCommand(buttonStates):
+	global currentButtonStates
+
+	if(buttonStates == currentButtonStates):
+		return
+
+	currentButtonStates = buttonStates
+	match buttonStates:
+		case [True, False, False, False]: 
+			sendCommand("forward")
+		case [True, True, False, False]: 
+			sendCommand("right")
+		case [True, False, False, True]: 
+			sendCommand("left")
+		case [False, True, False, False]: 
+			sendCommand("rotateRight")
+		case [False, False, False, True]: 
+			sendCommand("rotateLeft")
+		case [False, False, False, False]: 
+			sendCommand("stop")
+		case [False, False, True, False]: 
+			sendCommand("reverse")
+		case [False, True, True, False]: 
+			sendCommand("reverseRight")
+		case [False, False, True, True]: 
+			sendCommand("reverseLeft")
+		case [_,_,_,_]:
+			sendCommand("stop")
+
 run = True
 while run:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            print(pygame.key.name(event.key))
+	clock.tick(60)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+		if event.type == pygame.KEYDOWN:
+			print(pygame.key.name(event.key))
 
-    keys = pygame.key.get_pressed()
-    
-    rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
-    rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * vel
-        
-    rect.centerx = rect.centerx % window.get_width()
-    rect.centery = rect.centery % window.get_height()
+	keys = pygame.key.get_pressed()
 
-    window.fill(0)
-    pygame.draw.rect(window, (255, 0, 0), rect)
-    pygame.display.flip()
+	sendSomeCommand([keys[pygame.K_UP], keys[pygame.K_RIGHT], keys[pygame.K_DOWN], keys[pygame.K_LEFT]])
+	
+	rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * vel
+	rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * vel
+		
+	rect.centerx = rect.centerx % window.get_width()
+	rect.centery = rect.centery % window.get_height()
+
+	window.fill(0)
+	pygame.draw.rect(window, (255, 0, 0), rect)
+	pygame.display.flip()
 
 pygame.quit()
 exit()
